6a77a48fd1063a59e11365ea99f527ef69cc014c
bishwajit93 <bish.karm123@gmail.com>
2025-01-13 01:47:03 +0100

    urls.py changes

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from blog.views import PostViewSet
@@ -23,6 +24,7 @@ router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='post')
 
 urlpatterns = [
+    path('', lambda request: HttpResponseRedirect('/api/')),  # Redirect root URL to /api/
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]
